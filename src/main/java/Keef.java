ce90d78f66020a45829eddb890696c5e00e2f79f
keith <keithcheng.sgs@gmail.com>
2026-01-22 17:15:49 +0800

    Add Todo, Event and Deadline objects

--- src/main/java/Keef.java
+++ src/main/java/Keef.java
@@ -6,76 +6,83 @@ public class Keef {
     public static void main(String[] args) {
         // Scanner object to read input from the keyboard
         Scanner sc = new Scanner(System.in);
-        String user_input = "";
+        String userInput = "";
         List<Task> tasks = new ArrayList<>();
 
         // Hello greeting
-        draw_horizontal_line();
+        drawHorizontalLine();
         System.out.println("Hello! I'm Keef!\nWhat can I do for you? ~");
-        draw_horizontal_line();
+        drawHorizontalLine();
 
         // Conversation logic
         while (true) {
             System.out.print("You: ");
 
             // Get user input and vary response according to it
-            user_input = sc.nextLine();
-            String[] parts = user_input.split(" ");
+            userInput = sc.nextLine();
+            String[] parts = userInput.split(" ", 2);
             String command = parts[0];
+            String arguments  = parts.length > 1 ? parts[1] : "";
+
+            drawHorizontalLine();
+            System.out.print("Keef: ");
 
             switch (command) {
                 case "bye":
                     // Bye greeting
-                    draw_horizontal_line();
-                    System.out.print("Keef: ");
                     System.out.println("See ya soon! ~");
-                    draw_horizontal_line();
-                    break;
+                    drawHorizontalLine();
+                    return;
                 case "list":
                     // List out all items stored
-                    draw_horizontal_line();
                     System.out.println("Here are the tasks in your list:");
-                    print_tasks(tasks);
-                    draw_horizontal_line();
+                    printTasks(tasks);
+                    drawHorizontalLine();
                     continue;
                 case "mark":
                 case "unmark":
                     // Get the task number to be marked / unmarked
-                    int task_index = Integer.parseInt(parts[1]);
-                    Task selected_task = tasks.get(task_index - 1);
+                    int taskIndex = Integer.parseInt(arguments);
+                    Task selectedTask = tasks.get(taskIndex - 1);
 
                     // Mark a task as done or undone
-                    if (command.equals("mark")) {
-                        selected_task.markAsDone();
-                        draw_horizontal_line();
-                        System.out.println("Nice! I've marked this task as done:");
-                        System.out.println(selected_task);
-                        draw_horizontal_line();
+                    boolean done = command.equals("mark");
+                    if (done) {
+                        selectedTask.markAsDone();
                     } else {
-                        selected_task.markAsUndone();
-                        draw_horizontal_line();
-                        System.out.println("OK! I've marked this task as not done yet:");
-                        System.out.println(selected_task);
-                        draw_horizontal_line();
+                        selectedTask.markAsUndone();
                     }
 
+                    System.out.println("OK! I've marked this task as " + (done ? "done: " : "not done yet:"));
+                    System.out.println(selectedTask);
+                    drawHorizontalLine();
+                    continue;
+                case "todo":
+                    Task new_todo = new ToDo(arguments);
+                    tasks.add(new_todo);
+                    printAddMessage(new_todo, tasks.size());
+                    continue;
+                case "deadline":
+                    String[] deadlineParts = arguments.split("/by");
+                    Task deadline = new Deadline(deadlineParts[0], deadlineParts[1]);
+                    tasks.add(deadline);
+                    printAddMessage(deadline, tasks.size());
+                    continue;
+                case "event":
+                    String[] eventParts = arguments.split("/from|/to");
+                    Task event = new Event(eventParts[0], eventParts[1], eventParts[2]);
+                    tasks.add(event);
+                    printAddMessage(event, tasks.size());
                     continue;
                 default:
-                    // Add user_input to items list
-                    draw_horizontal_line();
-                    System.out.print("Keef: ");
-                    Task new_task = new Task(user_input);
-                    tasks.add(new_task);
-                    System.out.println("Added" + " " + "'" + new_task.description + "'");
-                    draw_horizontal_line();
-                    continue;
+                    // Invalid command
+                    System.out.println("Huh, what do you mean?");
+                    drawHorizontalLine();
             }
-
-            break;
         }
     }
 
-    public static void draw_horizontal_line() {
+    public static void drawHorizontalLine() {
         int length = 50;
         for (int i = 0; i < length; i++) {
             System.out.print("-");
@@ -83,9 +90,16 @@ public class Keef {
         System.out.println();
     }
 
-    public static void print_tasks(List<Task> tasks) {
+    public static void printTasks(List<Task> tasks) {
         for (int i = 0; i < tasks.size(); i++) {
             System.out.println(i+1 + "." + " " + tasks.get(i));
         }
+    }
+    
+    public static void printAddMessage(Task task, int size) {
+        System.out.println("Got it. I've added this task:");
+        System.out.println(task);
+        System.out.println("Now you have " + size + " tasks in your list.");
+        drawHorizontalLine();
     }
 }
